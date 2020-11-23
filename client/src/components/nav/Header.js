a253f6783409eeefb6eeba143c6759f03b36e5a1
Aleksey Belchenkov <u608110@gmail.com>
2020-11-23 16:35:15 +0300

    Link for Nav

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,27 +1,51 @@
 import React, { useState } from 'react';
 import { Menu } from 'antd';
-import { MailOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+    LoginOutlined,
+    HomeOutlined,
+    UserAddOutlined, SettingOutlined
+} from '@ant-design/icons';
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    const { SubMenu } = Menu;
-    const [current, setCurrent] = useState('');
+    const { SubMenu, Item } = Menu;
+    const [current, setCurrent] = useState('home');
 
     const handleClick = e => {
-        //setCurrent( e.key);
+        setCurrent(e.key);
     };
 
     return (
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-            <Menu.Item key="mail" icon={<MailOutlined />}>
-                Home
-            </Menu.Item>
-            <SubMenu
-                key="SubMenu"
-                icon={<SettingOutlined />}
-                title="Register"
+        <Menu
+            theme="dark"
+            onClick={handleClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+        >
+            <Item key="home" icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+            </Item>
+            <Item
+                className="float-right"
+                key="register"
+                icon={<UserAddOutlined />}
             >
-                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                <Menu.Item key="setting:2">Option 2</Menu.Item>
+                <Link to="/register">Register</Link>
+            </Item>
+            <Item
+                className="float-right"
+                key="login"
+                icon={<LoginOutlined />}
+            >
+                <Link to="/login">Login</Link>
+            </Item>
+
+            <SubMenu
+                icon={<SettingOutlined />}
+                title="Username"
+            >
+                <Item key="setting:1">Option 1</Item>
+                <Item key="setting:2">Option 2</Item>
             </SubMenu>
         </Menu>
     );
